ecd534b0d110eb0a74de81ab95d5c0326382e29b
Max <54718475+Fuhrer13@users.noreply.github.com>
2020-04-26 20:48:34 +0300

    Done

--- homework02.2/src/Helpers/filterContact.js
+++
@@ -1,8 +0,0 @@
-const filterContact = (contacts, filter) =>
-  contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase()),
-  );
-
-
-
-  export default filterContact;
