5a7eada1440df4933b1aa0c0670a3480b6133d0c
Max <54718475+Fuhrer13@users.noreply.github.com>
2020-04-26 21:07:38 +0300

    Updates

---
+++ precache-manifest.1162d7c107048c0dfd035c9d0fb3f60b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "073840d5e84da77bd6a2c7bef8a1a54d",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "7d1d416f2c953473e1d5",
+    "url": "/goit-react-hw-02-phonebook/static/css/2.f1d36a8f.chunk.css"
+  },
+  {
+    "revision": "0b2a386491c96b1dc0a6",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.20ab0746.chunk.css"
+  },
+  {
+    "revision": "7d1d416f2c953473e1d5",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.a51a04f7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.a51a04f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b2a386491c96b1dc0a6",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.2d1b4dce.chunk.js"
+  },
+  {
+    "revision": "6975b573071ed2324cbd",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.b2231cd6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Fuhrer13/goit-react-hw-02-phonebook.git/precache-manifest.17a6c74da6391edbff958a1671895729.js"
+  "/goit-react-hw-02-phonebook/precache-manifest.1162d7c107048c0dfd035c9d0fb3f60b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Fuhrer13/goit-react-hw-02-phonebook.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-02-phonebook/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
